96f71ec7e706ff2d6e104faf870ef3d6a01a90cd
Lucas <lucas.santos@mundowap.com.br>
2022-12-26 16:57:01 -0300

    feat: add new tests

--- reacttest/src/components/App.test.tsx
+++ reacttest/src/components/App.test.tsx
@@ -1,5 +1,20 @@
-describe("AppComponent", () => {
-  test("Ensure 1 + 1 to be 2", () => {
-    expect(1 + 1).toBe(2);
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App component", () => {
+  test("Ensure the App be render", () => {
+    render(<App />);
+  });
+
+  test("Make sure the App component has heading 1", () => {
+    render(<App />);
+    const h1 = screen.getByRole("heading", { level: 1 });
+    expect(h1).toEqual(screen.getByRole("heading", { level: 1 }));
+  });
+
+  test("Make sure the heading title is equal to React Teste", () => {
+    render(<App />);
+    const title = screen.getByRole("heading", { level: 1 });
+    expect(title.innerHTML).toBe(" React Teste ");
   });
 });
